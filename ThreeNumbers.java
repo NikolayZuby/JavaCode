f0e85d8714101b4d8d17f09ee668aace54e82eec
NikolayZuby <mr.zubiy49@gmail.com>
2016-02-10 00:42:20 +0300

    Create ThreeNumbers.java

---
+++ ThreeNumbers.java
@@ -0,0 +1,30 @@
+import java.io.*;
+/* Три числа
+Ввести с клавиатуры три целых числа. Определить, имеется ли среди них хотя бы одна пара равных между собой чисел.
+Если такая пара существует, вывести на экран числа через пробел. Если все три числа равны между собой, то вывести все три.
+Пример для чисел 1 2 2:
+2 2
+Пример для чисел 2 2 2:
+2 2 2
+*/
+public class ThreeNumbers
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        if (a == b & b == c)
+            System.out.println(a + " " + b + " " + c);
+        else
+        {
+            if (a == b)
+                System.out.println(a + " " + b);
+            if (b == c)
+                System.out.println(b + " " + c);
+            if (a == c)
+                System.out.println(a + " " + c);
+        }
+    }
+}
